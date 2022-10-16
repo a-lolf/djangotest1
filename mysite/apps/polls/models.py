056d33878c99d9f11f05a1caac9878efbd2fc68e
“amaan-ansari” <amaan.ansari@vfirst.com>
2022-10-16 20:19:47 +0530

    last

--- mysite/apps/polls/models.py
+++ mysite/apps/polls/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    objects = models.Manager()
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
